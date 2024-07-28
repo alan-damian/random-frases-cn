52a3150818caa0cbe3767da8da2e576fedbbddee
alan-damian <alan41.dc@gmail.com>
2024-07-28 14:54:59 +0200

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Suspense } from 'react';
+import { fetchData } from './fetchData';
 import './App.css'
 
+const apiData = fetchData("https://api.chucknorris.io/jokes/random")
+
 function App() {
-  const [count, setCount] = useState(0)
+
+  const data = apiData.read();
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>PRIMER PROYECTO VITE+REACT EN GH PAGES</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="App">
+      <h1> Fetch moderno</h1>
+      <Suspense fallback={<div>Loading...</div>}>
+        <ul>
+
+          <li key={data.id}>{data.value}</li>
+        </ul>
+      </Suspense>
+    </div>
   )
 }
 
