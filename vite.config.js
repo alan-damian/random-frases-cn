6329ba655254d50f18c79f57bae377b1325a1425
alan-damian <alan41.dc@gmail.com>
2024-07-28 12:46:53 +0200

    second commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://alan-damian.github.io/random-frases-cn"
 })
